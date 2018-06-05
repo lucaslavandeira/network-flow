677a9855b7e4f91c4ca635e86a32f019e16c15e9
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-06-04 21:13:04 -0300

    Agrego otro test

--- test/test_read.js
+++ test/test_read.js
@@ -9,7 +9,16 @@ describe("Camino", () => {
         let camino = caminos(g, "1", "5");
         let expected =  ["1", "2", "4", "5"];
         list_assert(expected, camino);
-    }); 
+    });
+
+    it("Devuelve un camino que empieza en el primer nodo y termina en el último", () => {
+        let g = readInputFile("test/inputs/dos_caminos");
+        let camino = caminos(g, "1", "4");
+
+        assert.equal(camino.length, 3);
+        assert.equal(camino[0], "1");
+        assert.equal(camino[2], "4");
+    })
 });
 
 function list_assert(expected, actual) {
